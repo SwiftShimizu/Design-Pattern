1c7305db11590d1725061a4560fe093ff630f6cd
SwiftShimizu <shimizuswiftforlearning@gmail.com>
2025-11-15 15:42:31 +0900

    Initial Commit

---
+++ Design Pattern/Design_PatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_PatternApp.swift
+//  Design Pattern
+//
+//  Created by 志水拓哉 on 2025/11/15.
+//
+
+import SwiftUI
+
+@main
+struct Design_PatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
